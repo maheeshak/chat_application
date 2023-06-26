8108a455e1f22cda772438fada27b0193f518b23
maheeshak <maheeshafernando18@gmail.com>
2023-06-26 14:45:47 +0530

    modify loading page

--- src/main/java/lk/ijse/chat_application/controller/LoadingWindowFormController.java
+++ src/main/java/lk/ijse/chat_application/controller/LoadingWindowFormController.java
@@ -1,9 +1,60 @@
 package lk.ijse.chat_application.controller;
 
+import javafx.animation.Animation;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
-public class LoadingWindowFormController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoadingWindowFormController implements Initializable {
     @FXML
     private ProgressBar loadingBar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                for (int i = 0; i <= 100; i++) {
+                    updateProgress(i, 55);
+                    Thread.sleep(55);
+                }
+                return null;
+            }
+        };
+
+
+        loadingBar.progressProperty().bind(task.progressProperty());
+        task.setOnSucceeded(event -> {
+            try {
+                Parent loginParent = FXMLLoader.load(getClass().getResource("/view/login_window_form.fxml"));
+                Scene loginScene = new Scene(loginParent);
+                Stage loginStage = new Stage();
+                loginStage.setResizable(false);
+                Image icon = new Image(getClass().getResourceAsStream("/view/img/logo.png"));
+                loginStage.getIcons().add(icon);
+                loginStage.setTitle("Chat Novo - Login");
+                loginStage.setScene(loginScene);
+                loginStage.show();
+
+                // Close the welcome stage
+                ((Stage) loadingBar.getScene().getWindow()).close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        new Thread(task).start();
+    }
+
 }
