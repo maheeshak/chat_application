fba4650eb0f7c3653d04e4f8ad76b303a27d4435
maheeshak <maheeshafernando18@gmail.com>
2023-06-26 11:04:31 +0530

    initial commit

---
+++ src/main/java/lk/ijse/chat_application/controller/AppInitializer.java
@@ -0,0 +1,24 @@
+package lk.ijse.chat_application.controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(this.getClass().getResource(""));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.setTitle("Novo");
+        stage.show();
+    }
+}
