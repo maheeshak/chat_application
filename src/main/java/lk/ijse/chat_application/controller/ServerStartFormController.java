8a6bdf48cfe2e397df3de0ee2494606b107a3f40
maheeshak <maheeshafernando18@gmail.com>
2023-06-26 14:47:23 +0530

    modify server start page

---
+++ src/main/java/lk/ijse/chat_application/controller/ServerStartFormController.java
@@ -0,0 +1,20 @@
+package lk.ijse.chat_application.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ServerStartFormController {
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    private JFXButton btnJoin;
+
+    @FXML
+    void btnJoimOnAction(ActionEvent event) {
+
+    }
+
+}
