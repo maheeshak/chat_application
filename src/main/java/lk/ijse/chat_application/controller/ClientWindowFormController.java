6f9382230966bb1f07d5e6eabe0bb515dde2194c
maheeshak <maheeshafernando18@gmail.com>
2023-06-26 14:47:49 +0530

    modify client window

---
+++ src/main/java/lk/ijse/chat_application/controller/ClientWindowFormController.java
@@ -0,0 +1,39 @@
+package lk.ijse.chat_application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class ClientWindowFormController {
+    @FXML
+    private TextField txtMsg;
+
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    private Label lblUser;
+
+    @FXML
+    void btnEmojiOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnImageOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSendOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtMsgOnAction(ActionEvent event) {
+
+    }
+
+}
