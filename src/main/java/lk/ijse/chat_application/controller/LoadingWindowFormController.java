9303c736eea04708bb4a9fd683180595cccb1c8f
maheeshak <maheeshafernando18@gmail.com>
2023-06-26 12:04:28 +0530

    create loading page

---
+++ src/main/java/lk/ijse/chat_application/controller/LoadingWindowFormController.java
@@ -0,0 +1,9 @@
+package lk.ijse.chat_application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+
+public class LoadingWindowFormController {
+    @FXML
+    private ProgressBar loadingBar;
+}
