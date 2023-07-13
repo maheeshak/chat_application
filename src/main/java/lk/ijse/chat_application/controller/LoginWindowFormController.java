3ef7fc1379f776cc7e94d086c9aee44b98877920
maheeshak <maheeshafernando18@gmail.com>
2023-07-13 13:01:22 +0530

    few changes

--- src/main/java/lk/ijse/chat_application/controller/LoginWindowFormController.java
+++ src/main/java/lk/ijse/chat_application/controller/LoginWindowFormController.java
@@ -5,14 +5,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import lk.ijse.chat_application.dto.Client;
+import lk.ijse.chat_application.bo.UserBO;
+import lk.ijse.chat_application.bo.UserBOImpl;
+import lk.ijse.chat_application.client.Client;
+import lk.ijse.chat_application.dto.UserDTO;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginWindowFormController {
+
+
+    UserBO userBO = new UserBOImpl();
+
+    @FXML
+    private AnchorPane root;
+
     @FXML
     private TextField txtUserName;
 
@@ -20,14 +34,68 @@ public class LoginWindowFormController {
     private JFXButton btnJoin;
 
     @FXML
+    private TextField txtPassword;
+
+    List<String> user_list = new ArrayList<>();
+
+    @FXML
     void btnJoinOnAction(ActionEvent event) {
         Client client = null;
+
         try {
+            if (!userBO.isExistUser(txtUserName.getText())) {
+                new Alert(Alert.AlertType.ERROR, "User is not registered :(").show();
+            } else {
 
-            client = new Client(txtUserName.getText());
+                if (!userBO.isValidUser(new UserDTO(txtUserName.getText(), txtPassword.getText()))) {
+                    new Alert(Alert.AlertType.ERROR, "User name or password is not valid :(").show();
+                } else {
+                    if (checkDuplicate(txtUserName.getText())) {
+                        new Alert(Alert.AlertType.ERROR,"User is already join this chat !").show();
+                    } else {
+                        user_list.add(txtUserName.getText());
+                        try {
 
-            Thread thread = new Thread(client);
-            thread.start();
+                            client = new Client(txtUserName.getText());
+
+                            Thread thread = new Thread(client);
+                            thread.start();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkDuplicate(String user_name) {
+
+        for (String name : user_list) {
+            if (name.equals(user_name)) {
+                return true;
+            }
+        }
+        return false;
+
+
+    }
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+        AnchorPane anchorPane = null;
+        try {
+            anchorPane = FXMLLoader.load(getClass().getResource("/view/user_register_window_form.fxml"));
+
+
+            Scene scene = new Scene(anchorPane);
+
+            Stage stage = (Stage) root.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Chat Novo");
+            stage.centerOnScreen();
         } catch (IOException e) {
             e.printStackTrace();
         }
