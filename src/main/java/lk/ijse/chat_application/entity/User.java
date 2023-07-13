0b2b04dedff9ce255d2e999bec7c7d6b877f6642
maheeshak <maheeshafernando18@gmail.com>
2023-07-13 13:00:40 +0530

    add user entity

---
+++ src/main/java/lk/ijse/chat_application/entity/User.java
@@ -0,0 +1,13 @@
+package lk.ijse.chat_application.entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+    private String user_name;
+    private String password;
+}
